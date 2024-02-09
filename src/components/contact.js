16e16328ce50cdc18b8d1b2f63821dd695f5353b
kimchieater <jasonj19276@gmail.com>
2024-02-09 14:58:49 +1100

    updated contact

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,9 +1,30 @@
 import './contact.css';
-import Nav from './nav'
+import {useRef} from 'react';
+import emailjs from '@emailjs/browser';
+import Nav from './nav';
 
 
 
 const Contacts = () => {
+    const form = useRef();
+
+    const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+    .sendForm('service_6umjf2h', 'template_1ivg25e', form.current, {
+        publicKey: 'e8e1OUi6XFp2pzWf3',
+    })
+        .then(
+        () => {
+            console.log('SUCCESS!');
+        },
+        (error) => {
+            console.log('FAILED...', error.text);
+        },
+        );
+    };
+
     return(
         <div className="contacts">
             <Nav></Nav>
@@ -12,15 +33,15 @@ const Contacts = () => {
 
                     <h2>Let's talk</h2>
 
-                    <form name="contact" method="POST" data-netlify="true">
+                    <form ref={form} onSubmit={sendEmail}>
 
-                        <label>Name<input type="text" name="name" className='name' required></input></label>
+                        <label>Name<input type="text" name="user_name" className='name' required></input></label>
                         
-                        <label>Email<input type="email" name='email' className='email' required></input></label>
+                        <label>Email<input type="email" name='user_email' className='email' required></input></label>
                         
-                        <label>Your interest <input type="text" name="interest" className="interest"></input></label>
+                        <label>Your interest <input type="text" name="message" className="interest"></input></label>
 
-                        <label>Additional Information  <textarea name="info" className='info'></textarea></label>
+                        <label>Additional Information  <textarea name="message" className='info'></textarea></label>
 
                         <button type="submit">Submit</button>
                     </form>
